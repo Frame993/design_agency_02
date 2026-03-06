54f5efd273463f20f47cf0e905be7a8119cea1da
Yordani Alvarez <yalvarezb93@gmail.com>
2026-03-06 14:47:03 +0300

    chore: Update `brilo-api` script path to `.mjs` and add `cwd` in ecosystem config.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'brilo-api',
-      script: './apps/api/dist/index.js',
+      script: './apps/api/dist/index.mjs',
+      cwd: '/var/www/design_agency_02/apps/api',
       env: { NODE_ENV: 'production' },
       restart_delay: 3000,
       max_restarts: 10,
